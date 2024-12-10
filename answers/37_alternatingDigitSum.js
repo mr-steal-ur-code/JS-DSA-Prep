eb880269db88bb2aa6e0d243e68e693c9f498ac8
CJ Karkut <cjkarkut@gmail.com>
2024-12-10 04:27:00 -0600

    add alternating digit sum

---
+++ answers/37_alternatingDigitSum.js
@@ -0,0 +1,23 @@
+// Given a sequence of digits, calculate the alternating sum of the digits in the list. The alternating sum is defined as the sum of digits at even indices (0-based) minus the sum of digits at odd indices.
+
+// Input: 1234
+
+// Digits at even indices: 1, 3 → Sum = 1 + 3 = 4
+// Digits at odd indices: 2, 4 → Sum = 2 + 4 = 6
+// Alternating sum = 4 - 6 = -2
+// Output: -2
+
+const alternatingDigitSum = (digits) => {
+	let odd = 0,
+		even = 0;
+	for (let i = 0; i < digits.length; i++) {
+		if (i % 2 === 0) {
+			even += Number(digits[i]);
+		} else odd += Number(digits[i]);
+	}
+	return even - odd;
+};
+
+console.log(alternatingDigitSum("1234")); //-2
+console.log(alternatingDigitSum("987654321")); // 5
+console.log(alternatingDigitSum("456789")); //-3
---
+++ questions/37_alternatingDigitSum.js
@@ -0,0 +1,16 @@
+// Given a sequence of digits, calculate the alternating sum of the digits in the list. The alternating sum is defined as the sum of digits at even indices (0-based) minus the sum of digits at odd indices.
+
+// Input: 1234
+
+// Digits at even indices: 1, 3 → Sum = 1 + 3 = 4
+// Digits at odd indices: 2, 4 → Sum = 2 + 4 = 6
+// Alternating sum = 4 - 6 = -2
+// Output: -2
+
+const alternatingDigitSum = (digits) => {
+
+};
+
+console.log(alternatingDigitSum("1234")); //-2
+console.log(alternatingDigitSum("987654321")); // 5
+console.log(alternatingDigitSum("456789")); //-3
